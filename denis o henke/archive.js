45a02f2969851f3d80c909f66e17ed5fb2566f5f
BuddhistBurrito <henrik.ekroth@gmail.com>
2020-06-26 16:57:18 +0200

    Add files via upload

---
+++ denis o henke/archive.js
@@ -0,0 +1,70 @@
+document.write ("<p>Text by JS</p>");
+
+// ROADMAP:
+// if statement
+// objects
+// arrays
+// functions
+// functions specific for arrays
+// interaction between HTML and JavaScript
+
+
+// let choice = confirm("Do you really want to do this?");
+// if (choice === true) {
+//   alert("yeey going ahead with the thing");
+// } else {
+//   alert("ok FINE i won’t do it");
+// }
+
+// let plant = {
+//   name: "Monstera",
+//   price: 50.0
+// }
+
+// alert("The price of " + plant.name + " is PRICE_HERE");
+// alert(`The price of ${plant.name} is ${plant.price}`);
+
+// if (plant.price < 20) {
+//   alert("cheap af");
+// } else {
+//   alert("expensive i guess");
+// }
+
+// ?:     <---- this is called the ternary operator 
+// alert(plant.price < 20 ? "cheap af" : "expensive i guess");
+
+// if (plant.price < 20) {
+//   alert("cheap af")
+// } else if (plant.price < 50) {
+//   alert("okay price i guess")
+// } else {
+//   alert("expensive af")
+// }
+
+// let prices = [20, 50, 70];
+// prices[0] -----> this would be 20
+// prices[1] -----> this would be 50
+// prices[2] -----> this would be 70
+
+// for (let index = 0; index < prices.length; index++) {
+//   alert(price[index])
+// }
+// prices.forEach((theRealPrice) 
+// => {
+//   alert(theRealPrice)
+// })
+// prices.forEach(function (theRealPrice) {
+//   alert(theRealPrice)
+// })
+
+// let plants = [
+//   { name: "Monstera", price: 50.0, sellers: ["denis", "henrik"] },
+//   { name: "Spider plant", price: 40.0, sellers: ["benoit"] },
+// ]
+// // plants[0] -----> { name: "Monstera", price: 50.0 }
+
+// plants.forEach((p) => {
+//   p.sellers.forEach((theSeller) => {
+//     console.log(`${p.name} is sold by ${theSeller} for €${p.price}`);
+//   })
+// });
